3a33f68913d3a702a3b9f87be87595226574f8c3
huerta13 <tonohuerta691@gmail.com>
2025-04-30 21:09:59 -0600

    Update

---
+++ Backend/src/main/java/com/vantus/project/model/Alumno.java
@@ -0,0 +1,34 @@
+package com.vantus.project.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Alumno")
+
+public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_alumno")
+    private Integer idAlumno;
+
+    @Column(name = "matricula", nullable = false)
+    private String matricula;
+
+    @Column(name = "semestre", nullable = false)
+    private Integer semestre;
+
+    @Column(name = "grupo", nullable = false)
+    private String grupo;
+
+    @ManyToOne
+    @JoinColumn(name = "id_horario")
+    private Horario_Sala horario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    //Falta tabla Horario_sala
+
+}
