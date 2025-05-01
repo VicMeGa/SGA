7c4ecba46b5667ef056bfc04da163c64bdbe5d2a
huerta13 <tonohuerta691@gmail.com>
2025-05-01 14:05:38 -0600

    Login corregido

--- Backend/src/main/java/com/vantus/project/controller/LoginController.java
+++ Backend/src/main/java/com/vantus/project/controller/LoginController.java
@@ -2,8 +2,6 @@ package com.vantus.project.controller;
 
 import com.vantus.project.dto.LoginRequest;
 import com.vantus.project.dto.LoginResponse;
-//import com.vantus.project.model.Administrativo;
-//import com.vantus.project.model.Usuario;
 import com.vantus.project.repository.AdministrativoRepository;
 import com.vantus.project.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +25,6 @@ public class LoginController {
                 .filter(admin -> admin.getContrasena().equals(request.getContrasena()))
                 .map(admin -> new LoginResponse("Inicio de sesión exitoso", true))
             )
-            .orElse(new LoginResponse("Credenciales incorrectas o usuario no es administrativo", false));
+            .orElse(new LoginResponse("Credenciales incorrectas.", false));
     }
 }
