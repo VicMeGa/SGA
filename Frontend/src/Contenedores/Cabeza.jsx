3b18c29c4b887de35aa325b66c9e7bf03c4df735
TheeBunny <uliseshvasquez17@gmail.com>
2025-05-22 00:07:31 -0600

    [TheBunny] Logo VANTUS va a la pagina de menu :)

--- Frontend/src/Contenedores/Cabeza.jsx
+++ Frontend/src/Contenedores/Cabeza.jsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import vantus from "../recursos/vantus.png"
 
 function Cabeza (){
+    const navigate = useNavigate();
+
     return(
         <>
         <header className='arriba'>
-            <img className="logo" src={vantus}/>
+            <img className="logo" src={vantus}  alt="Logo de Vantus"  onClick={() => navigate("/next")}
+                style={{ cursor: "pointer" }}
+/>
         </header>
         <br/>
         <br />
